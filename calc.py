ce54e58f17e2c9cf84b370435ef55bfb4db11dd7
MariaSkazina <artacama@gmail.com>
2019-09-24 16:52:33 +0300

    Calc.py in english

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
-a = float(input())
-x = str(input())
-b = float(input())
+a = float(input('Enter your first number '))
+x = str(input('Enter operator '))
+b = float(input('Enter your second number '))
 if x == '+':
     print(a + b)
 elif x == '-':
@@ -8,16 +8,16 @@ elif x == '-':
 elif (x == '/') and (b != 0):
     print(a / b)
 elif (x == '/') and (b == 0):
-    print('Деление на 0!')
+    print('Division by 0!')
 elif x == '*':
     print(a * b)
 elif (x == 'mod') and (b != 0):
     print(a % b)
 elif (x == 'mod') and (b == 0):
-    print('Деление на 0!')
+    print('Division by 0!')
 elif x == 'pow':
     print(a ** b)
 elif (x == 'div') and (b != 0):
     print(a // b)
 elif (x == 'div') and (b == 0):
-    print('Деление на 0!')
+    print('Division by  0!')
