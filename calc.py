afdfd6cc433b5b9a1a7f1ac9f3b907520a28b1d1
MariaSkazina <artacama@gmail.com>
2019-09-24 16:23:25 +0300

    Fixed calc.py

--- calc.py
+++ calc.py
@@ -2,22 +2,23 @@ a = float(input())
 x = str(input())
 b = float(input())
 if x == '+':
-  print(a + b)
+    print(a + b)
 elif x == '-':
-  print(a - b)
+    print(a - b)
 elif (x == '/') and (b != 0):
-  print(a / b)
+    print(a / b)
 elif (x == '/') and (b == 0):
-  print('Деление на 0!')
+    print('Деление на 0!')
 elif x == '*':
-  print(a*b)
-elif (x == 'mod') and (b!=0):
-  print(a%b)
-elif (x=='mod') and (b==0):
-  print('Деление на 0!')
+    print(a * b)
+elif (x == 'mod') and (b != 0):
+    print(a % b)
+elif (x == 'mod') and (b == 0):
+    print('Деление на 0!')
 elif x == 'pow':
-  print(a**b)
-elif (x == 'div') and (b!=0):
-  print(a//b)
-elif (x=='div') and (b==0):
-  print('Деление на 0!')
+    print(a ** b)
+elif (x == 'div') and (b != 0):
+    print(a // b)
+elif (x == 'div') and (b == 0):
+    print('Деление на 0!')
+
